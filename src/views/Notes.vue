8e2507ca2910021090beb16071419653bf52bb6b
mhubl <hubala.mateusz@gmail.com>
2020-04-22 07:17:59 +0200

    Only the currently authenticated user's notes are now displayed

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="notes">
       <note
-        v-for="note in unfilteredNotes"
+        v-for="note in notes"
         v-bind:key="note.id"
         v-bind:title="note.title"
         v-bind:text="note.text"
@@ -21,8 +21,8 @@
 
 <script>
 import note from '../components/Note.vue'
-// import * as firebase from 'firebase/app'
-// import 'firebase/auth'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import { db } from '../database'
 
 export default {
@@ -30,51 +30,42 @@ export default {
   components: { note },
   data () {
     return {
-      dummynotes: [
-        {
-          id: 'AAAAA',
-          title: 'Test title 1',
-          text: 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ',
-          author: 'Test author',
-          created: new Date(2020, 1, 17, 13, 21, 23).toUTCString()
-        },
-        {
-          id: 'BBBBB',
-          title: 'Test title 2',
-          text: 'short example',
-          author: 'Test author',
-          created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
-        },
-        {
-          id: 'CCCCC',
-          title: 'Another test note',
-          text: 'This note is a bit longer than the second, but still shorter than the first one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one',
-          author: 'Test author',
-          created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
-        },
-        {
-          id: 'DDDDD',
-          title: 'test',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis justo at nisi consequat, ut tempus tellus venenatis. Nunc eu laoreet odio, in ultrices ligula. Nulla imperdiet turpis a elit tristique porttitor. Integer diam tortor, sagittis finibus viverra id, porta vel lorem. Aenean eu fringilla tortor, eget sollicitudin arcu. Nulla sagittis libero in laoreet congue. Maecenas aliquam id massa vitae consequat. Etiam mattis sapien et enim varius, sed facilisis arcu lacinia. Donec quis libero sit amet eros aliquam facilisis. Morbi. ',
-          author: 'Test author',
-          created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
-        }
-      ],
-      unfilteredNotes: []
+      // dummynotes: [
+      //   {
+      //     id: 'AAAAA',
+      //     title: 'Test title 1',
+      //     text: 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ',
+      //     author: 'Test author',
+      //     created: new Date(2020, 1, 17, 13, 21, 23).toUTCString()
+      //   },
+      //   {
+      //     id: 'BBBBB',
+      //     title: 'Test title 2',
+      //     text: 'short example',
+      //     author: 'Test author',
+      //     created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
+      //   },
+      //   {
+      //     id: 'CCCCC',
+      //     title: 'Another test note',
+      //     text: 'This note is a bit longer than the second, but still shorter than the first one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one one',
+      //     author: 'Test author',
+      //     created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
+      //   },
+      //   {
+      //     id: 'DDDDD',
+      //     title: 'test',
+      //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis justo at nisi consequat, ut tempus tellus venenatis. Nunc eu laoreet odio, in ultrices ligula. Nulla imperdiet turpis a elit tristique porttitor. Integer diam tortor, sagittis finibus viverra id, porta vel lorem. Aenean eu fringilla tortor, eget sollicitudin arcu. Nulla sagittis libero in laoreet congue. Maecenas aliquam id massa vitae consequat. Etiam mattis sapien et enim varius, sed facilisis arcu lacinia. Donec quis libero sit amet eros aliquam facilisis. Morbi. ',
+      //     author: 'Test author',
+      //     created: new Date(2020, 3, 2, 8, 56, 39).toUTCString()
+      //   }
+      // ],
+      notes: []
     }
   },
-  firestore: {
-    unfilteredNotes: db.collection('notes')
-  },
-  computed: {
-    notes: function () {
-      const res = []
-      for (const note in this.notes) {
-        if (note.author.uid === this.$store.state.user.uid) {
-          res.push(note)
-        }
-      }
-      return res
+  firestore () {
+    return {
+      notes: db.collection('notes').where('author', 'array-contains', db.doc('users/' + firebase.auth().currentUser.uid))
     }
   }
 }
