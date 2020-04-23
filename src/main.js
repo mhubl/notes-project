51359c0d292cc867b919b7201e7db0227e3e2e8e
mhubl <hubala.mateusz@gmail.com>
2020-04-23 19:41:06 +0200

    Fixed auto authentication

--- src/main.js
+++ src/main.js
@@ -11,26 +11,40 @@ Vue.config.productionTip = false
 Vue.use(firestorePlugin)
 
 // eslint-disable-next-line no-unused-vars
-const app = new Vue({
-  el: '#app',
-  router,
-  store,
-  methods: {
-    signOut: function () {
-      this.$store.dispatch('userSignOut')
-      firebase.auth().signOut()
-      if (router.currentRoute.path !== '/') {
-        router.push('/')
-      }
-    },
-    registerAccount: function (uid, userData) {
-      db.collection('users').doc(uid).set(userData)
+let app
+firebase.auth().onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      store,
+      methods: {
+        signOut: function () {
+          this.$store.dispatch('userSignOut')
+          firebase.auth().signOut()
+          if (router.currentRoute.path !== '/') {
+            router.push('/')
+          }
+        },
+        registerAccount: function (uid, userData) {
+          db.collection('users').doc(uid).set(userData)
+        }
+      },
+      firebase: function () {
+        return {
+          currUser: db.doc(`users/${firebase.auth().currentUser.uid}`)
+        }
+      },
+      render: h => h(App)
+    })
+    if (user) {
+      db.collection('users').doc(user.uid).get()
+        .then(userSnap => {
+          app.$store.dispatch('userSignIn', userSnap.data())
+          if (app.$router.currentRoute.path !== '/notes') {
+            app.$router.push('/notes')
+          }
+        })
     }
-  },
-  firebase: function () {
-    return {
-      currUser: db.doc(`users/${firebase.auth().currentUser.uid}`)
-    }
-  },
-  render: h => h(App)
+  }
 })
